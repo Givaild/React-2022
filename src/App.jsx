dee9d0d977386b9aa95a184b2c469377bed42fbd
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-06-10 14:34:30 -0300

    study props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import {Post} from './Post'
 
 
-function App() {
+export function App() {
   
 
   return (
-    <h1>Hello Word</h1>
+    <div>
+    <Post 
+      author="Givaildo de Andrade" 
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione voluptates sequi nam atque cumque, provident delectus. Amet fugit sint perferendis doloribus, cumque libero, quis porro, tenetur sed quisquam voluptatem dolor."
+      />
+      <Post
+        author="Gustavo"
+        content= "Tenho 7 anos" 
+      />
+      <Post
+        author="Henrique"
+        content= "Tenho 7 anos" 
+      />
+   
+    </div>
   )
 }
 
-export default App
