4a4148e4e37bb83ca5eb78fc3393bf659d7020cc
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-07-21 09:51:51 -0300

    feat: insert useState to create new comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import { format, formatDistanceToNow } from "date-fns"
 import ptBR from "date-fns/locale/pt-BR"
+import { useState } from "react"
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"
@@ -9,6 +10,12 @@ import styles from "./Post.module.css"
 
 
 export function Post ({author,publishedAt,content}) {
+const [comments,setComments] = useState([
+    1,
+    2,
+    
+])
+
     const publishedDateFormated = format(publishedAt,"d 'de' LLLL 'às' HH:mm'h'",{
         locale:ptBR,
     })
@@ -17,6 +24,13 @@ export function Post ({author,publishedAt,content}) {
         locale:ptBR,
         addSuffix:true,
     })
+
+    function handleCreateNewComment () {
+        event.preventDefault();
+
+        setComments([...comments,comments.length+1]);
+       
+    }
     
     return (
         <article className={styles.post}>
@@ -47,7 +61,7 @@ export function Post ({author,publishedAt,content}) {
 
             </div>
 
-        <form className={styles.commentForm}>
+        <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
             <strong>Deixe seu feedback</strong>
 
             <textarea placeholder="Deixe um comentário" />
@@ -58,11 +72,13 @@ export function Post ({author,publishedAt,content}) {
             </footer>
 
         </form>
-        <div className={styles.commentList}>
-            <Comment />
-            <Comment />
-            <Comment />
-        </div>
+
+
+             <div className={styles.commentList}>
+            {comments.map(comment => {
+                return <Comment />
+            })}
+            </div>
         </article>
     )
 }
