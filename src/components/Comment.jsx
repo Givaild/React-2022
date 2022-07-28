9d519624aa7e750d69fc7826e882d41919289253
Givaildo de Andrade <givaildo.gliveira@synvia.com>
2022-07-28 13:56:38 -0300

    Making likes in the comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,21 +1,30 @@
 import { ThumbsUp, Trash } from "phosphor-react"
+import { useState } from "react";
 import { Avatar } from "./Avatar"
 import styles from "./Comment.module.css"
 
 export function  Comment ({content,onDeleteComment}) {
+
+    const [likeCout, setLikeCount] = useState(0);
+
     function hadleDeleteComment() {
         onDeleteComment(content);
     
     }
+
+    function handleLikeComment() {
+        setLikeCount(likeCout + 1);
+    }
+
     return (
         <div className={styles.comment}>
-        <Avatar hasBorder={false} src="https://github.com/Givaild.png" alt="" />
+        <Avatar hasBorder={false} src="https://github.com/felipethecosta.png" alt="" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
-                            <strong>Givaildo de Andrade</strong>
+                            <strong>Felipe Costa</strong>
                             <time title="15 de Junho de às 12:00"  dateTime="2022-06-15 12:00:20">Cerca de 1h atrás </time>                           
                         </div>
                         
@@ -28,9 +37,9 @@ export function  Comment ({content,onDeleteComment}) {
                 </div>
                 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCout}</span>
                     </button>
                 </footer>
 
